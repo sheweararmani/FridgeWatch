9be400e1e35398c515e17cd4ad00935abc482e4b
Arlyn <acrylami@gmail.com>
2018-11-25 11:46:25 +0000

    Changes from David
    Recipe.api added, items updated, gui button for delete

--- items.py
+++ items.py
@@ -1,81 +1,81 @@
 import csv
 import datetime
-from dateutil import parser
 global ListboxList
-def find_item_date_and_name(item_id,items_access_copy,good_food_types):
+
+
+def find_item_date_and_name(item_id, items_access_copy, good_food_types):
 
     i = 0
     arr = []
     for row in items_access_copy:
-        #print(row)
+        # print(row)
         if i == 0:
             i = i + 1
         else:
-            if int(row[0]) == item_id and row[2] in good_food_types:#we have found the item, now access the exp date
-                exp_date = parser.parse(row[3])
+            # we have found the item, now access the exp date
+            if int(row[0]) == item_id and row[2] in good_food_types:
+                exp_date = datetime.datetime.strptime(row[3], "%Y-%m-%d")
                 name = row[1]
                 arr.append(exp_date)
                 arr.append(name)
     return arr
+
+
 def start(ID):
-    good_food_types = ["Fruit", "Beverages", "Baking", "Vegetables", "Condiments", "Meats","Breads & Cereal", "Entrees","Diary","Snacks","Desserts","Seafood","Candy","snacks","Baby food","Legumes","Soups","Sauces","Sides"]
+    good_food_types = ["Fruit", "Beverages", "Baking", "Vegetables", "Condiments", "Meats", "Breads & Cereal", "Entrees",
+                       "Diary", "Snacks", "Desserts", "Seafood", "Candy", "snacks", "Baby food", "Legumes", "Soups", "Sauces", "Sides"]
     ListboxList = []
     items = open("ProductDatabase.csv", "r")
     users = open("users.csv", "r")
     items_access = csv.reader(items)
     items_access_copy = []
     users_access = csv.reader(users)
-    user_id = int(ID)#This will depend on the USER!!! CHANGE ME IN THE GUI
+    user_id = int(ID)  # This will depend on the USER!!! CHANGE ME IN THE GUI
     user_items = []
     for row in items_access:
         items_access_copy.append(row)
     i = 0
     for row in users_access:
         if i == 0:
-            i = i + 1 #IGNORE THE HEADER
+            i = i + 1  # IGNORE THE HEADER
         else:
-            if int(row[0]) == user_id:#we have found the user, access their items
-                exp_date_and_name = find_item_date_and_name(int(row[1]),items_access_copy,good_food_types)
+            if int(row[0]) == user_id:  # we have found the user, access their items
+                exp_date_and_name = find_item_date_and_name(
+                    int(row[1]), items_access_copy, good_food_types)
                 current_date = datetime.datetime.now()
                 print(exp_date_and_name)
                 if exp_date_and_name != []:
-                    ListboxList.append(str(exp_date_and_name[1]) + " will expire in " + str((exp_date_and_name[0] - current_date).days) + " days")
+                    ListboxList.append(str(exp_date_and_name[1]) + " will expire in " + str(
+                        (exp_date_and_name[0] - current_date).days) + " days")
                 print(ListboxList)
 
     return ListboxList
 
-#New code below this 
-ITEMS = "ProductDatabase.csv"
-USERS = "users.csv"
 
-USER_ID = 0 # This will depend on the USER!!! CHANGE ME IN THE GUI
-user_items = []
-
-def delete_item(item_id):
+def delete_item(user_id, item_id, file_name="users.csv"):
 
     data = []
 
     # open and reader every row of the csv file
-    with open(USERS,newline="") as csvfile:
+    with open(file_name, newline="") as csvfile:
         item_reader = csv.reader(csvfile, delimiter=',')
         for i, row in enumerate(item_reader):
             print(row)
-                
+
             # skip the column headers
             if i == 0:
                 data.append(row)
                 continue
 
             # skip the data to be deleted
-            if int(row[0]) == int(USER_ID) and int(row[1]) == int(item_id):
+            if int(row[0]) == int(user_id) and int(row[1]) == int(item_id):
                 continue
 
             # append the data to keeep
             data.append(row)
 
-    # write the data to the USERS csv file
-    with open(USERS, 'w', newline="") as f:
+    # write the data to the csv file
+    with open(file_name, 'w', newline="") as f:
         writer = csv.writer(f)
         for row in data:
             writer.writerow(row)
-##New Code Above This
